17a38f686d01a97c38c7fa780985ab5820c4a163
clementpeiry <peiryclement@gmail.com>
2024-11-12 16:20:35 +0100

    les bases

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
 print("Projet Pygame")
+
+import pygame
+from pygame.locals import *
+from random import randint
+
+pygame.init()
+LARGEUR = 600
+HAUTEUR = 600
+fenetre = pygame.display.set_mode((LARGEUR, HAUTEUR))
+clock = pygame.time.Clock()
+continuer = True
+
+perso = pygame.sprite.Sprite()
+pygame.sprite.Sprite.__init__(perso)
+perso.image = pygame.image.load("Diplodocus_personnage.png").convert_alpha()
+perso.rect = perso.image.get_rect()
+perso.rect.x = 50
+perso.rect.y = 50
+
+liste_des_sprites = pygame.sprite.LayeredUpdates()
+liste_des_sprites.add(perso)
+
+direction = "droite"
+
+while continuer:
+    liste_des_sprites.draw(fenetre)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+    fenetre.fill((0, 0, 0))
+    clock.tick(60)
+
+pygame.quit()
